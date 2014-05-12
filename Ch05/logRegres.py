fcd88a14aa57d377a087bb79d96caaa80efebd4c
uberdugo <uriel.berdugo@wepingo.com>
2014-05-12 17:40:12 +0200

    Update logRegres.py

--- Ch05/logRegres.py
+++ Ch05/logRegres.py
@@ -35,7 +35,7 @@ def gradAscent(dataMatIn, classLabels):
 
         weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
         if int(k % 25) == 0:
-            vect = (inner(dataMatrix.transpose(),weights))
+            vect = dataMatrix*weights
             #print ("vect : \n")
             #print(vect)
             #scal = dot(vect.transpose(), vect)
